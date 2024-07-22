67440204a9b5e1333adb7c9773cb2e539ae2b668
twingqywingqy <rendra.zurd04@gmail.com>
2024-07-22 07:53:29 +0700

    feat: memisahkan elemen ke komponen sendiri

--- src/App.vue
+++ src/App.vue
@@ -19,13 +19,13 @@ watch(search, () => {
 			<input v-model.trim="search" type="text" id="search-input" />
 		</header>
 		<section id="quiz-container">
-			<div v-for="quiz in quizes" :key="quiz.id" class="card">
+			<!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
 				<img :src="quiz.img" :alt="quiz.title" />
 				<div class="card-body">
 					<h2>{{ quiz.title }}</h2>
 					<p>{{ quiz.questions.length }} Questions</p>
 				</div>
-			</div>
+			</div> -->
 		</section>
 	</main>
 </template>
@@ -59,29 +59,5 @@ header {
 	display: flex;
 	flex-wrap: wrap;
 	margin-top: 20px;
-}
-
-.card {
-	width: 270px;
-	margin-right: 30px;
-	margin-bottom: 30px;
-	border-radius: 5px;
-	overflow: hidden;
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-.card img {
-	width: 100%;
-	height: 200px;
-	object-fit: cover;
-	margin: 0;
-}
-
-.card-body {
-	padding: 0 20px;
-}
-
-.card-body h2 {
-	font-weight: bold;
 }
 </style>
